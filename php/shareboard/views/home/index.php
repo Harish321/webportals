9720aa8514797865813a9e9bf589511e9e0e3ba7
Harish Dhulipalla <harish.d14@iiits.in>
2018-06-06 09:43:52 +0530

    php

---
+++ php/shareboard/views/home/index.php
@@ -0,0 +1,8 @@
+<div class="text-center">
+	<h1>Welcome To Viswa Bharathi College</h1>
+	<p class="lead">Find something cool? Share it with our community. Look at other shares as well</p>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>Students">Students</a>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>Staff">Staff</a>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>Fees">Fee</a>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>Accounts">Accounts</a>
+</div>
